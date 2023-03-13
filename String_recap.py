59f340e40e5f0582922d40338eceedba395112db
pedropablomolina <ppmolinaarol@gmail.com>
2023-03-13 14:05:47 +0100

    Initial commit

---
+++ String_recap.py
@@ -0,0 +1,17 @@
+text = "I like cats"
+
+print(text)
+print(text[0], text[7])
+print(text[7:])
+print(text)
+#text = text[7:] + "sneaky r" + text[8:]
+#
+#print("I produce this result:", text)
+print(f"the result of 2+3= {2+3}")
+print(f"I produce this result:{text[:7]}sneaky r{text[8:]}")
+
+
+
+
+
+
